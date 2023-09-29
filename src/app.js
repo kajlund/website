a986618ce1d4d509b0765dc729d438e57728c59d
Kaj Lund <kaj.lund@gmail.com>
2023-09-29 22:59:03 +0300

    Adds Edge templating and a home/index page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
+import path from 'node:path'
+
 import express from 'express'
+import engine from 'express-edge'
 
 import db from './db.js'
+import indexRoutes from './routes/index.js'
 
 const app = express()
 
 // Middleware for url encoding
 app.use(express.urlencoded({ extended: false }))
+
+app.use(express.static('public'))
+
+// View engine
+app.use(engine);
+app.set('views', path.join(process.cwd(), 'views'))
+app.disable('view cache')
+
+app.use('/', indexRoutes)
 
 // Connect to DB
 db.connect()
---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import crypto from 'node:crypto'
+
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+  const user = {
+    email: 'user@gmail.com'
+  }
+
+  try {
+    const hash = crypto.createHash('md5').update(user.email).digest('hex')
+    const avatar = `https://www.gravatar.com/avatar/${hash}?s=36`
+
+    res.render('home', {
+      pageTitle: 'Welcome!',
+      isLoggedIn: false,
+      avatar,
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+export default router
